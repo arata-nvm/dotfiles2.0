5cfe7ca873403ab51a312e3a4c266c28cfa1b85e
Arata <arata04962@gmail.com>
2023-07-15 00:56:06 +0900

    feat: ignore lid switch

--- hosts/hishikui/configuration.nix
+++ hosts/hishikui/configuration.nix
@@ -22,6 +22,10 @@
 
   services.tailscale.enable = true;
 
+  services.logind.extraConfig = ''
+    HandleLidSwitch=ignore
+  '';
+
   time.timeZone = "Asia/Tokyo";
 
   i18n.defaultLocale = "en_US.UTF-8";
